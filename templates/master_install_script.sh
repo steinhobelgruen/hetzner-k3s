5f1724be70b22f7f4d2c3bfa736799a2d7395c70
Thomas Renger <dentaku@wazong.de>
2023-01-21 21:23:15 +0100

    Start k3s in DualStack-mode
    (sadly does not work until Hetzner virtual networks support IPv6 routing)

--- templates/master_install_script.sh
+++ templates/master_install_script.sh
@@ -9,6 +9,8 @@ HOSTNAME=$(hostname -f)
 PRIVATE_IP=$(ip route get 10.0.0.1 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 PUBLIC_IP=$(hostname -I | awk '{print $1}')
 NETWORK_INTERFACE=$(ip route get 10.0.0.1 | awk -F"dev " 'NR==1{split($2,a," ");print a[1]}')
+PRIVATE_IP6=$(ip -6 addr show dev ${NETWORK_INTERFACE} scope link | awk -F"inet6 " 'NR==2{split($2,a,"/");print a[1]}')
+PUBLIC_IP6=$(ip -6 addr show scope global | awk -F"inet6 " 'NR==2{split($2,a,"/");print a[1]}')
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_TOKEN="{{ k3s_token }}" INSTALL_K3S_EXEC="server \
 --disable-cloud-controller \
@@ -18,7 +20,8 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_TOKEN
 --disable metrics-server \
 --write-kubeconfig-mode=644 \
 --node-name=$HOSTNAME \
---cluster-cidr=10.244.0.0/16 \
+--cluster-cidr=10.244.0.0/16,fdfc:215a:ea98::/56 \
+--service-cidr=10.243.0.0/16,fdfc:215a:ea98:1::/112 \
 --etcd-expose-metrics=true \
 {{ flannel_wireguard }} \
 --kube-controller-manager-arg="bind-address=0.0.0.0" \
@@ -27,8 +30,8 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_TOKEN
 {{ taint }} {{ extra_args }} \
 --kubelet-arg="cloud-provider=external" \
 --advertise-address=$PRIVATE_IP \
---node-ip=$PRIVATE_IP \
---node-external-ip=$PUBLIC_IP \
+--node-ip=$PRIVATE_IP,$PRIVATE_IP6 \
+--node-external-ip=$PUBLIC_IP,$PUBLIC_IP6 \
 --flannel-iface=$NETWORK_INTERFACE \
 {{ server }} {{ tls_sans }}" sh -
 
--- templates/worker_install_script.sh
+++ templates/worker_install_script.sh
@@ -9,12 +9,14 @@ HOSTNAME=$(hostname -f)
 PRIVATE_IP=$(ip route get 10.0.0.1 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 PUBLIC_IP=$(hostname -I | awk '{print $1}')
 NETWORK_INTERFACE=$(ip route get 10.0.0.1 | awk -F"dev " 'NR==1{split($2,a," ");print a[1]}')
+PRIVATE_IP6=$(ip -6 addr show dev ${NETWORK_INTERFACE} scope link | awk -F"inet6 " 'NR==2{split($2,a,"/");print a[1]}')
+PUBLIC_IP6=$(ip -6 addr show scope global | awk -F"inet6 " 'NR==2{split($2,a,"/");print a[1]}')
 
 curl -sfL https://get.k3s.io | K3S_TOKEN="{{ k3s_token }}" INSTALL_K3S_VERSION="{{ k3s_version }}" K3S_URL=https://{{ first_master_private_ip_address }}:6443 INSTALL_K3S_EXEC="agent \
 --node-name=$HOSTNAME \
 --kubelet-arg="cloud-provider=external" \
---node-ip=$PRIVATE_IP \
---node-external-ip=$PUBLIC_IP \
+--node-ip=$PRIVATE_IP,$PRIVATE_IP6 \
+--node-external-ip=$PUBLIC_IP,$PUBLIC_IP6 \
 --flannel-iface=$NETWORK_INTERFACE" sh -
 
 systemctl start k3s-agent # on some OSes the service doesn't start automatically for some reason
